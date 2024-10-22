cf5284691976a4c5f934166815af93b18577fb88
gkraud4135 <77388722+gkraud4135@users.noreply.github.com>
2024-10-22 20:47:03 +0900

    new project :: tocknock

---
+++ src/main/java/com/app/tocknock/TocknockApplication.java
@@ -0,0 +1,13 @@
+package com.app.tocknock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocknockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocknockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tocknock/TocknockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tocknock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocknockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
